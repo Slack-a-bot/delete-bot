565ae6180f0b17129b0c9b5d7ee7b10a44884867
micjun <jung.michael@protonmail.com>
2022-04-20 12:56:29 -0400

    feat: delete placeholder

--- server/server.js
+++ server/server.js
@@ -11,6 +11,17 @@ const app = new App({
   // appToken: process.env.APP_TOKEN,
 });
 
+app.message(/^(delete|remove).*/, async ({ context, say }) => {
+  try {
+    const result = await client.chat.delete({
+      channel: channelId,
+      ts: messageId,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
